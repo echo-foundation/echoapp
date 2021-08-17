9f17463326f3bb1bdb6965c1dfb76e77c26a6073
Charles Yebuah <chokyere0@gmail.com>
2021-08-17 11:09:05 +0000

    created the functional one way chat

--- functions.js
+++ functions.js
@@ -1,29 +1,64 @@
-//function changestyle() {
-	//var text = document.getElementById ("para1").style.backgroundColor = "blue"; 
-	//var text = document.getElementById ("para1").style.fontStyle = "italic"; 
+			let messages = new Array
+			let body = ''
 
-//function changestyle() {
-//	var paragraph = document.getElementsByTagName("p");
-//for (var i = 0 ; i < paragraph.length; i++) {
-//	paragraph[i].style.fontStyle = "italic";
-//}
-//}
-	// var changeParaText = paragraph[2].style.fontStyle = "italic";
-//function changestyle() {
-//	var paragraph = document.getElementsByClassName('para');
+			function addMessage(){
+				let obj = new Object
+				obj.id = messages.length + 1
+				obj.body = body
+				obj.time = "12: 02 pm"
+				obj.read = true
+				messages.push(obj)
+				testDiv.innerHTML= ''
+				obj.from = obj.id % 2 > 0 ? "him" : "me"
 
-  //  var changeText = paragraph[0].innerHTML = "you saved me thanks"
- //   var changeText = paragraph[1]. innerHTML ="you saved me too"   
-//}
+				let chatContainer = document.getElementById('chatContainer')
+				chatContainer.insertAdjacentHTML('beforeend',  `<div class="sp"></div>
+					<div class="mess ${ obj.from === 'me' ? 'mess-r' : 'mess-l'}">
+					<p>${obj.body}</p>
+					<div class="check">
+					<span>${obj.time}</span>
+					<img src="assets/check-2.png">
+					</div>
+					</div>`)
 
-function changestyle(){
-	var paragraph = document.getElementsByClassName('para');
+				/** 
+				 * Create new div element with class .chat-r or .chat-l based on the content of from 
+				 * Inject div with cla
+				 * 
+				 * 
+				 * 
+				 * 	<div class="chat-r" >	
+						<div class="sp"></div>
+						<div class="mess mess-r">
+							<p id="mbody">
+								<img src="assets/dots.png" width= 10px>
+							</p>
+							<div class="check">
+								<span>4:00 pm</span>
+								<img src="assets/check-2.png">
+							</div>
+						</div>				
+					</div>
+					* **/
 
-var firstparatext = paragraph[0].innerHTML;
-var secondparatext = paragraph[1].innerHTML;
-var addThem = paragraph[2].innerHTML = firstparatext + secondparatext;
+				}
 
-var firstparatext = paragraph[0].innerHTML= " ";
-var secondparatext = paragraph[1].innerHTML=" ";
 
-}
+				// document.getElementById("mbody").innerHTML = "yayyy";
+			// 	//select something- [GET ELEMENT][$("whatever tag")]
+			// 	//append a child element to the selected parent[]
+			// 	//inject the html to the newly appended
+
+			const testDiv = document.getElementById('testdiv')
+
+			testDiv.addEventListener('input', function(event){
+				body = event.target.innerHTML
+			})
+			testDiv.addEventListener("keypress", function(event) {
+				if (event.keyCode === 13) {
+					event.preventDefault()
+					document.getElementById("sendButton").click()
+					testDiv.innerHTML = ''
+				}
+			})
+
